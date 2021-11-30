fc88c27bc99ed915e4511c8e470646d35dec9b92
g3tx <g3tx@mail.ru>
2021-11-30 19:56:32 +0300

    Initial commit

---
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -0,0 +1,41 @@
+package hiber.dao;
+
+import hiber.model.User;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
+
+    @Override
+    public User getUserByCar(String model, int series) {
+        String HQL = "FROM User u WHERE u.car.series=:series AND u.car.model=:model";
+        Query query = sessionFactory.getCurrentSession().createQuery(HQL, User.class);
+        query.setParameter("series", series);
+        query.setParameter("model", model);
+        User user = (User) query.getSingleResult();
+        return user;
+    }
+
+
+}
---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,51 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+
+    @OneToOne(mappedBy = "car")
+    private User user;
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "series")
+    private int series;
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    public Car() {
+    }
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    @Override
+    public String toString() {
+        return  model + " " + series;
+    }
+}
